f081d837a767ed8ffd422574725c12195a20e96a
Demchenko Yevhenii <ed.@lineup.com.ua>
2018-03-30 14:36:48 +0200

    fixed bug

--- js/common.js
+++ js/common.js
@@ -1,107 +1,195 @@
-function showSection(dataResult) {
+(function($) {
+    let maksiservices = {
+        'showSection': function showSection(param) {
+            let currentElemIdent = '';
+            let self = this;
+            console.log('into showSection');
+            // $('.step').hide();
+            if (param) {
+                $('.card-device').unbind('click');
+                $('.card-brand').unbind('click');
+            }
 
-    // eventListener for step1 devices
-    $('.card-device').on('click', function () {
-        const selectorStep = '#step-2';
-        const identification = $(this).attr('data-device');
-        dataResult.device = identification;
-        // if($(this).hasClass('active')) {
-        //     $('.step').hide('slow');
-        //     $(this).removeClass('active');
-        // }
-            // todo: create new functionality
-            // $('.card-device').addClass('active');
-        // if ($(this).hasClass('card card-device')) {
-            // call of function render DOM
-            createDOMStep('data-brand', brands, dataResult);
+            // eventListener for step1 devices
+            $('.card-device').on('click', function () {
+                const selectorStep = '#step-2';
+                const identification = $(this).attr('data-device');
 
-            $(selectorStep).toggle( "slow").scroll();
-        // }
-
-
-
-
-
-    });
-    // eventListener for step2 brands
-    $('.card-brand').on('click', function () {
-        console.log('step2', dataResult);
-        const selectorStep = '#step-3';
-        const identification = $(this).attr('data-brand');
-        if ($(this)[0].className === 'card card-brand') {
-            createDOMStep('data-problems', problems, dataResult);
-            // showSection(dataResult);
-            $(selectorStep).toggle( "slow" ).scroll();
-        }
+                self.result.device = identification;
+                console.log(currentElemIdent);
+                if (currentElemIdent !== '' && currentElemIdent === identification) {
+                    currentElemIdent = $(this).attr('data-device');
+                    $('.card-device').unbind('click');
+                    console.log('if');
+                    // проверить и скрить секцию
+                    $(selectorStep).toggle( "slow");
+                    // self.createDOMStep();
+                } else {
+                    currentElemIdent = $(this).attr('data-device');
+                    $('.card-device').unbind('click');
+                    // просто создам елементы
+                    console.log('else');
+                    self.createDOMStep('data-brand', brands);
+                    $(selectorStep).hide( "slow");
+                    $(selectorStep).show( "slow");
+                }
 
 
-    });
-    // eventListener for step3 problems
-    $('.card-problem').on('click', function () {
-        const selectorStep = '#step-4';
-        const identification = $(this).attr('data-problem');
-        if ($(this)[0].className === 'card card-problem') {
-            $(selectorStep).toggle( "slow" ).scroll();
-        }
-    });
+            });
 
-}
+            // eventListener for step2 brands
+            $('.card-brand').on('click', function () {
+                // debugger
+                console.log('step2');
+                const selectorStep = '#step-3';
+                const identification = $(this).attr('data-brand');
+                if ($(this)[0].className === 'card card-brand') {
+                    self.createDOMStep('data-problems', problems);
+                    $(selectorStep).toggle( "slow" );
+                }
 
-function createDOMStep(identification1, data, dataResult) {
 
-    if(identification1 === 'data-brand'){
-        let content = '';
+            });
 
-        data.forEach( item => {
-            let contentString = `<div class="card card-brand" data-brand="${item.name}">
+            // eventListener for step3 problems
+            $('.card-problem').on('click', function () {
+                console.log('sdsds');
+                const selectorStep = '#step-4';
+                const identification = $(this).attr('data-problem');
+                self.result.problemId = identification;
+                console.log(self.result.problemId);
+                if ($(this)[0].className === 'card card-problem') {
+                    console.log(self.result);
+                    // self.createDOMStep('data-problems', problems);
+                    $(selectorStep).hide( "slow" );
+                    self.createResult();
+                    $(selectorStep).show( "slow" );
+                }
+            });
+
+        },
+        'createDOMStep': function (identification, data) {
+            const self = this;
+            // debugger
+            // console.log(problems);
+            if(identification === 'data-brand'){
+                let content = '';
+                $('.holder-brands').contents().remove();
+                data.forEach( item => {
+                    let contentString = `<div class="card card-brand" data-brand="${item.name}">
                             <div>
                                 <img src="${item.img}" alt="${item.name}">
                             </div>
                         </div>`;
-            content = content + contentString;
-        });
+                    content = content + contentString;
+                });
+                $('.holder-brands').append(content);
+                this.showSection(true);
+            }
 
-        $('.holder-brands').append(content);
-
-
-
-    }
-
-    if(identification1 === 'data-problems'){
-        console.log('data-problem');
-        let content = '';
-        console.log(dataResult.device);
-        data.forEach( item => {
-            if(item.identification !== undefined && item.identification === dataResult.device){
-                console.log('item.identification', item.identification);
-                console.log('idevice', dataResult.device);
-                let contentString = `<div class="card card-problem" data-problem="${item}">
+            if(identification === 'data-problems'){
+                console.log('data-problem');
+                let content = '';
+                $('.holder-problems').contents().remove();
+                // console.log(dataResult.device);
+                data.forEach( item => {
+                    if(item.identification !== undefined && item.identification === self.result.device){
+                        console.log('item.identification', item.identification);
+                        // console.log('idevice', dataResult.device);
+                        let contentString = `<div class="card card-problem" data-problem="${item.id}">
                                     <div>
                                         <span class="title">${item.problem}</span>
                                     </div>
                                 </div>`;
-                content = content + contentString;
+                        content = content + contentString;
+                    }
+
+                });
+
+                $('.holder-problems').append(content);
+                this.showSection(true);
+
+
+
             }
 
-        });
-
-        $('.holder-problems').append(content);
 
 
+        },
+        createResult: function createResult() {
+            let self = this;
+            devices.forEach( item => {
+                if (item.name === self.result.device) {
+                    self.result.title = item.title;
+                    self.result.img = item.img;
 
-    }
+                }
+            });
+            problems.forEach( item => {
+                if (item.id === +self.result.problemId) {
+                    self.result.titleProblem = item.problem;
+                    console.log(item.price);
+                    self.result.price = item.price;
+                    self.result.restriction = item.restriction;
 
-    showSection(dataResult);
-}
 
-$(document).ready(function () {
-    let result = {
-        device: '',
-        problem: '',
-        restriction: '',
-        price: 0,
-        img: ''
+                }
+            })
 
+            this.createDOMElementofResult();
+
+        },
+        createDOMElementofResult: function createDOMElementofResult() {
+            $('.order-holder').contents().remove();
+            let content = `<div class="description-device">
+                                    <div class="image-device">
+                                        <img src="${this.result.img}" alt="img">
+                                    </div>
+                                    <div class="details-device">
+                                        <div>
+                                            <div class="type">Тип техники</div>
+                                            <div class="type-name">${this.result.title}</div>
+                                        </div>
+                                        <div>
+                                            <div class="type">Неисправность</div>
+                                            <div class="type-name">${this.result.titleProblem}</div>
+                                        </div>
+                                        <div class="${(this.result.restriction === undefined)? 'restoring-none' : 'restoring'}">
+                                            <span>${this.result.restriction}</span>
+                                        </div>
+                                        <div class="repair-price">
+                                            <span class="price-title">Сумма ремонта</span>
+                                            <span class="price">${this.result.price} руб</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="order-form">
+                                    <p>Заполните ваши контактные данные
+                                        и мы презвоним вам в течении 5 минут</p>
+                                    <form action="/">
+                                        <div class="form-holder">
+                                            <input type="text" placeholder="Ваше имя">
+                                            <input type="number" placeholder="Ваш телефон">
+                                            <input type="submit" value="Отправить заявку на ремонт">
+                                        </div>
+                                    </form>
+                                </div>`;
+
+            $('.order-holder').append(content);
+        },
+    result: {
+            title: '',
+            titleProblem: '',
+            device: '',
+            problemId: '',
+            restriction: '',
+            price: 0,
+            img: ''
+        },
     };
-    showSection(result);
-});
+
+    $(document).ready(function(){
+        maksiservices.showSection(false);
+    });
+
+})(jQuery);
